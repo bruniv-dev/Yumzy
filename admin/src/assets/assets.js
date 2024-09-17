4a70eee168951ed0e839fb5e6379ed7a09a70ade
= <bruniv.dev@gmail.com>
2024-09-17 17:14:45 +0530

    npm i and admin assets

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,17 @@
+import add_icon_white from "./add_icon_white.png";
+import add_icon_yellow from "./add_yellow.png";
+import bag_yellow from "./bag_yellow.png";
+import logo_dark from "./logo-dark.png";
+import logo from "./logo.png";
+import parcel_icon from "./parcel_icon.png";
+import profile_icon from "./profile_icon.png";
+
+export const assets = {
+  add_icon_white,
+  add_icon_yellow,
+  bag_yellow,
+  logo_dark,
+  logo,
+  parcel_icon,
+  profile_icon,
+};
