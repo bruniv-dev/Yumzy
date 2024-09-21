15a8e13ee4797cd6225050386f92f025f718a38c
= <bruniv.dev@gmail.com>
2024-09-22 00:57:37 +0530

    order model

---
+++ backend/models/order.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  items: {
+    type: Array,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: Object,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Food Processing",
+  },
+  date: {
+    type: date,
+    default: date.Now(),
+  },
+  payment: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const orderModel = mongoose.models.order || mongoose.model("order", order);
+export default orderModel;
