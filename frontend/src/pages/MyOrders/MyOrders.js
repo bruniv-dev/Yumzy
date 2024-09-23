9aa7913f97b4139511e3a37b1fd73aa7409bdb8d
= <bruniv.dev@gmail.com>
2024-09-23 13:30:48 +0530

    my-order-page-responsive

--- frontend/src/pages/MyOrders/MyOrders.js
+++ frontend/src/pages/MyOrders/MyOrders.js
@@ -45,7 +45,7 @@ const MyOrders = () => {
                 <span>&#x25cf; </span>
                 <b>{order.status}</b>
               </p>
-              <button>Track Order</button>
+              <button>Track</button>
             </div>
           );
         })}
